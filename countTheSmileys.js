beef89a43835ee933d6833bf702e18188b9f618f
chris.bridges <christopher_bridges@icloud.com>
2018-08-25 13:13:17 -0400

    added valid smiley face hash

---
+++ countTheSmileys.js
@@ -0,0 +1,27 @@
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+  // create data array of all valid smileys
+    const validSmileys = [];
+  //   loops through each validSmiley to see if current arg value is equal to a valid
+  //     doesn't scale particularly well if need to add more smileys
+  
+  // using Regex
+  //   see if first character is valid 'eyes' - ":" or ";"
+    //   check if nose is present - "-" or "~"
+    //   if it is, next char must be mouth - ")" or "D"
+    
+  // if eyes
+  //   if nose
+  //     if mouth
+  //   if mouth
+  // DRY mouth logic
+  
+  // I can't seem to figure out the Regex, can I just use Char Codes?
+  // Create hash map for all valid charcodes for eyes, noses, mouths?
+  const validSmileysHash = {
+    eyes: [58, 59],
+    nose: [45, 126],
+    mouth: [41, 68]
+  }
+  
+  }
