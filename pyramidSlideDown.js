d79b9a6d714bf89c6e14c4fbb27bff3c76f64302
chris.bridges <christopher_bridges@icloud.com>
2018-09-03 13:00:36 -0400

    this solution works for the small examples, but doesnt quite seem to scale for some reason

---
+++ pyramidSlideDown.js
@@ -0,0 +1,23 @@
+function longestSlideDown (pyramid) {
+  //   take the index of the the start, and then, take the highest value from the next array +/- 1 from previous index
+  // slide-left value === index of previous array, slide-right === index of previous array + 1
+    let currentSlideIndex = 0
+    let sum = pyramid[0][0]
+    for (let i = 1; i < pyramid.length; i++) {
+      let nextSlideValue
+      let leftSlideValue = pyramid[i][currentSlideIndex]
+      let rightSlideValue = pyramid[i][currentSlideIndex + 1]
+      if (leftSlideValue > rightSlideValue) {
+        nextSlideValue = leftSlideValue
+        currentSlideIndex = currentSlideIndex > 0 ? currentSlideIndex-- : currentSlideIndex
+        sum += nextSlideValue
+        console.log(nextSlideValue, sum)
+      } else {
+        nextSlideValue = rightSlideValue
+        currentSlideIndex++
+        sum += nextSlideValue
+        console.log(nextSlideValue, sum)
+      }
+    }
+    return sum
+  }
