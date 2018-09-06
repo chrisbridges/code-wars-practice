a7116b8dd6fa3e679ea2c0f67753ae51087bf755
chris.bridges <christopher_bridges@icloud.com>
2018-09-06 13:47:23 -0400

    pseudocode implemented and basic idea - implementation of block func needs to be architected

---
+++ sudokuDoneOrNot.js
@@ -0,0 +1,19 @@
+function doneOrNot(board){
+  // loop board arrays and check that all includes 1 through 9
+  // create new arrays out of "blocks" and run through same func
+  
+    function contains1through9 (arr) {
+      for (let i = 1; i <= 9; i++) {
+        if (!arr.includes(i)) {
+          return false
+        }
+      }
+    }
+    
+    const blockArrays = []
+    
+    for (let i = 0; i < board.length; i++) {
+      
+    }
+    
+  }
