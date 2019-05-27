514eda0bdb8981a0fd9a7e2b0da7f232388d887d
Chris Bridges <chrisbridges@Chriss-MacBook-Pro.local>
2019-05-27 12:12:47 -0400

    initial commit - trying to understand the problem

---
+++ airportRotors.js
@@ -0,0 +1,5 @@
+var flapDisplay = function(lines, rotors) {
+  console.log('lines',lines,'rotors',rotors)
+  const rotorValues = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ?!@#&()|<>.:=-+*/0123456789'
+  return lines
+}
