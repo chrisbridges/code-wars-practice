f08aa0ec1268af8dc3a413955219fef1e88668a2
Chris Bridges <christopher_bridges@icloud.com>
2019-05-27 13:10:06 -0400

    misunderstood the problem - come back later

--- airportRotors.js
+++ airportRotors.js
@@ -1,18 +1,19 @@
 var flapDisplay = function(lines, rotors) {
   console.log('lines',lines,'rotors',rotors)
+  console.log(ALPHABET)
 //   this is a string of unique characters, so we only need to find the first instance of any character
-  const rotorValues = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ?!@#&()|<>.:=-+*/0123456789'
+//   const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 //   for each value in lines, loop over them
 //   for each corresponding value in rotors, move that value forward in rotor values by X rotor value
-//   find index of character in rotorValues, from there, add rotor to index
+//   find index of character in ALPHABET, from there, add rotor to index
 //     to avoid indexing out, check if value + currentIndex is longer than length
 //       if it is, subtract value by remaning indexes and try again
 
 // lines is an array with a single value
   const splitLines = lines[0].split('')
-  const lastIndexOfRotorValues = rotorValues.length - 1
+  const lastIndexOfRotorValues = ALPHABET.length - 1
   splitLines.forEach((lineValue, index) => {
-    const indexOfLineValue = rotorValues.indexOf(lineValue)
+    let indexOfLineValue = ALPHABET.indexOf(lineValue)
     console.log(indexOfLineValue)
     let numberOfCharsToMoveForward = rotors[0][index]
 //     probably put this in a while loop in case of any really large rotor values
@@ -21,10 +22,14 @@ var flapDisplay = function(lines, rotors) {
       console.log(numberOfCharsToMoveForward)
       
     }
-    lineValue = rotorValues[indexOfLineValue + numberOfCharsToMoveForward]
+    lineValue = ALPHABET[indexOfLineValue + numberOfCharsToMoveForward]
     console.log(lineValue)
     splitLines[index] = lineValue
   })
   console.log(splitLines)
-  return splitLines
+  return [splitLines.join('')]
 }
+
+// OH, each rotor flip advances every character by 1, not just the individual character...
+// I "believe" my solution would have worked had I properly understood the rotation method
+// come back to this at another time...
