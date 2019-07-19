ba03b9c097b2b891bf811ac6e6375ba5f2bea646
Chris Bridges <christopher_bridges@icloud.com>
2019-07-19 17:54:45 -0400

    solved problem with some commentary

--- product-of-all-others-hard.ts
+++ product-of-all-others-hard.ts
@@ -4,6 +4,20 @@
 
 // Follow-up: what if you can't use division?
 
+function productOfAllOthers (arr) {
+  const result = []
+  for (let i = 0; i < arr.length; i++) {
+    // you need to create a copy of the array to get the values of it, instead of the reference
+      // setting the array as freshArr = arr, will access the same spot in memory and modify the original arr array
+      // see: https://www.samanthaming.com/tidbits/35-es6-way-to-clone-an-array
+    let freshArr = [...arr]
+    let arrSansIndex = removeIndex(freshArr, i)
+    let productOfNewArr = reduceAllIndexes(arrSansIndex)
+    result.push(productOfNewArr)
+  }
+  return result
+}
+
 function reduceAllIndexes (arr) {
   return arr.reduce((acc, num) => acc * num)
 }
@@ -13,14 +27,6 @@ function removeIndex (arr, index) {
   return arr
 }
 
-function productOfAllOthers (arr: number[]) {
-  let freshArr = arr
-  for (let i = 0; i < arr.length; i++) {
-    // let freshArr = arr
-    console.log(freshArr)
-    let arrSansIndex = removeIndex(freshArr, i)
-    console.log(arrSansIndex)
-  }
-}
+console.log(`${productOfAllOthers([3, 2, 1])} should equal [2, 3, 6]`)
+console.log(`${productOfAllOthers([1, 2, 3, 4,5])} should equal [120, 60, 40, 30, 24]`)
 
-productOfAllOthers([1,2,3])
