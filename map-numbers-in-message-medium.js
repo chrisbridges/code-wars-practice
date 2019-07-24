320f72c03d0a2a611a3b7b18a7b6cad9c3b92597
Chris Bridges <christopher_bridges@icloud.com>
2019-07-24 07:42:06 -0400

    added some notes

---
+++ map-numbers-in-message-medium.js
@@ -0,0 +1,13 @@
+// This problem was asked by Facebook.
+
+// Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+// For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+// You can assume that the messages are decodable. For example, '001' is not allowed.
+
+// Notes:
+  // you know that for every number, you can use letters for 1-9 and iterate a letter for every digit
+  // since the largest possible combination would be 2 numbers combined, for every 2 numbers you could combine and then iterate as well as every possible combination
+
+  
